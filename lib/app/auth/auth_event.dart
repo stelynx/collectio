05018692ed76441051406533ebbf24a11a927758
campovski <campovski@gmail.com>
2020-04-03 20:09:38 +0200

    SLCOL-12: Implement auth part of BLoC

--- lib/app/auth/auth_event.dart
+++ lib/app/auth/auth_event.dart
@@ -1,7 +1,10 @@
 part of 'auth_bloc.dart';
 
-abstract class AuthEvent {
+abstract class AuthEvent extends Equatable {
   const AuthEvent();
+
+  @override
+  List<Object> get props => [];
 }
 
 class CheckStatusAuthEvent extends AuthEvent {}
