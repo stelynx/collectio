cf2a2d85d8dbe77e373e145c221991487d188483
campovski <campovski@gmail.com>
2020-04-03 12:12:28 +0200

    SLCOL-11: Improve UserProfile coverage

--- test/core/model/user_profile_test.dart
+++ test/core/model/user_profile_test.dart
@@ -3,7 +3,7 @@ import 'package:collectio/core/utils/constant/enum/country.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('should be able to equate two UserProfile instances', () {
+  test('should two UserProfile instances with same userUid be equal', () {
     final UserProfile userProfile1 = UserProfile(
       email: 'a@b.co',
       firstName: 'firstName',
@@ -24,5 +24,29 @@ void main() {
     );
 
     expect(userProfile1, equals(userProfile2));
+  });
+
+  test('should two UserProfile instances with same userUid have same hash code',
+      () {
+    final UserProfile userProfile1 = UserProfile(
+      email: 'a@b.co',
+      firstName: 'firstName',
+      lastName: 'lastName',
+      country: Country.SI,
+      profileImg: '',
+      userUid: '123456',
+      username: 'username',
+    );
+    final UserProfile userProfile2 = UserProfile(
+      email: 'a@b.co',
+      firstName: 'firstName',
+      lastName: 'lastName',
+      country: Country.SI,
+      profileImg: '',
+      userUid: '123456',
+      username: 'username',
+    );
+
+    expect(userProfile1.hashCode, equals(userProfile2.hashCode));
   });
 }
