aca0551eee0ab33db83e3397f9acbcfb450e4c42
campovski <campovski@gmail.com>
2020-04-03 07:31:49 +0200

    SLCOL-8: Add FirebaseAuthService

---
+++ lib/core/service/auth_service.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+
+abstract class AuthService {
+  Future signInWithEmailAndPassword(
+      {@required String email, @required String password});
+
+  Future signUpWithEmailAndPassword(
+      {@required String email, @required String password});
+
+  Future logout();
+}
