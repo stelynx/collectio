13a29b72863df5d7c1b1af9b76437fbdf6598ec8
campovski <campovski@gmail.com>
2020-04-07 10:04:29 +0200

    SLCOL-15: Fix coverage_test_creator.sh

--- scripts/coverage_test_creator.sh
+++ scripts/coverage_test_creator.sh
@@ -4,4 +4,7 @@ file=test/coverage_test.dart
 
 echo "// ignore_for_file: unused_import" > $file
 find lib -name '*.dart' -and -not -name '*_event.dart' -and -not -name '*_state.dart' | cut -c4- | awk -v package=collectio '{printf "import '\''package:collectio%s'\'';\n", $1}' >> $file
-echo "\nvoid main(){}" >> $file
+echo "void main(){}" >> $file
+
+echo "File with following content created!"
+cat $file;
