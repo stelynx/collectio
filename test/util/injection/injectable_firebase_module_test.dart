4ac72cfaf7a39e727d2f562a66d0b85cae729740
campovski <campovski@gmail.com>
2020-04-14 10:55:47 +0200

    SLCOL-19: Add test for production injection

--- test/util/injection/injectable_firebase_module_test.dart
+++ test/util/injection/injectable_firebase_module_test.dart
@@ -1,17 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:collectio/util/injection/injection.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:injectable/injectable.dart' show Environment;
 
 void main() {
+  // This line is necessary in order to obtain Firestore.
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  configureInjection(Environment.prod);
+
   group('firebaseAuth', () {
     test('should return FirebaseAuth.instance in production', () {
-      configureInjection(Environment.prod);
-
       // Do not type result, otherwise the test has no point!
       final result = getIt<FirebaseAuth>();
 
       expect(result.runtimeType.toString(), equals('FirebaseAuth'));
+    });
+  });
+
+  group('firestore', () {
+    test('should return Firestore.instance in production', () {
+      // Do not type result, otherwise the test has no point!
+      final result = getIt<Firestore>();
+
+      expect(result.runtimeType.toString(), equals('Firestore'));
     });
   });
 }
