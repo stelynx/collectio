9a00f0ed0991d627bee3b0af9e44213e9056f9e7
Marcel Čampa <campovski@gmail.com>
2020-04-15 14:50:19 +0200

    SLCOL-24: Add UserProfile capabilities to DataService (#47)

--- test/service/firebase/firebase_data_service_test.dart
+++ test/service/firebase/firebase_data_service_test.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:collectio/model/user_profile.dart';
 import 'package:collectio/service/firebase/firebase_data_service.dart';
+import 'package:collectio/util/constant/constants.dart';
 import 'package:collectio/util/injection/injection.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:injectable/injectable.dart' show Environment;
@@ -11,6 +13,12 @@ void main() {
   configureInjection(Environment.test);
 
   final String username = 'username';
+  final String userUid = 'userUid';
+  final MockedCollectionReference mockedCollectionReference =
+      MockedCollectionReference();
+  final MockedDocumentReference mockedDocumentReference =
+      MockedDocumentReference();
+
   FirebaseDataService firebaseDataService;
 
   setUp(() {
@@ -21,7 +29,7 @@ void main() {
     test('should call Firestore.collection with <username>_collection',
         () async {
       when(firebaseDataService.firestore.collection(any))
-          .thenReturn(MockedCollectionReference());
+          .thenReturn(mockedCollectionReference);
 
       await firebaseDataService.getCollectionsForUser(username);
 
@@ -36,7 +44,7 @@ void main() {
         () async {
       final String collectionName = 'collectionName';
       when(firebaseDataService.firestore.collection(any))
-          .thenReturn(MockedCollectionReference());
+          .thenReturn(mockedCollectionReference);
 
       await firebaseDataService.getItemsInCollection(
           username: username, collectionName: collectionName);
@@ -45,5 +53,52 @@ void main() {
               .collection("${username}_collection/$collectionName/items"))
           .called(1);
     });
+  });
+
+  group('getUserProfile', () {
+    test(
+      'should call Firestore.collection with correct collection and where clause',
+      () async {
+        when(firebaseDataService.firestore.collection(any))
+            .thenReturn(mockedCollectionReference);
+        when(mockedCollectionReference.where(any,
+                isEqualTo: anyNamed('isEqualTo')))
+            .thenReturn(MockedCollectionReference());
+
+        await firebaseDataService.getUserProfile(userUid: userUid);
+
+        verify(mockedCollectionReference.where(
+          Constants.userUidField,
+          isEqualTo: userUid,
+        )).called(1);
+      },
+    );
+  });
+
+  group('addUserProfile', () {
+    test(
+      'should call Firestore with correct collection, document and setData',
+      () async {
+        final Map<String, dynamic> userProfileJson = UserProfile(
+          email: 'email',
+          userUid: userUid,
+          username: username,
+        ).toJson();
+
+        when(firebaseDataService.firestore.collection(any))
+            .thenReturn(mockedCollectionReference);
+        when(mockedCollectionReference.document(any))
+            .thenReturn(mockedDocumentReference);
+        when(mockedDocumentReference.setData(any))
+            .thenAnswer((_) async => null);
+
+        await firebaseDataService.addUserProfile(
+            id: userProfileJson['username'], userProfile: userProfileJson);
+
+        verify(mockedCollectionReference.document(userProfileJson['username']))
+            .called(1);
+        verify(mockedDocumentReference.setData(userProfileJson)).called(1);
+      },
+    );
   });
 }
