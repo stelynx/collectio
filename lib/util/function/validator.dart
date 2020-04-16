f8d93c752fcdccde1558541fd9f989ab8ac92d06
Marcel Čampa <campovski@gmail.com>
2020-04-16 19:54:44 +0200

    SLCOL-28: Add registered user's profile to Firestore (#52)

--- lib/util/function/validator.dart
+++ lib/util/function/validator.dart
@@ -6,26 +6,30 @@ class Validator {
   static Either<ValidationFailure, String> isValidEmail(String email) {
     if (email == '') return Left(EmailEmptyValidationFailure());
 
-    RegExp emailRegex = RegExp(
+    final String trimmedEmail = email.trim();
+    final RegExp emailRegex = RegExp(
         r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+$");
-    return emailRegex.hasMatch(email)
-        ? Right(email.trim())
+    return emailRegex.hasMatch(trimmedEmail)
+        ? Right(trimmedEmail)
         : Left(EmailValidationFailure());
   }
 
   static Either<ValidationFailure, String> isValidPassword(String password) {
-    RegExp passwordRegex = RegExp(r"^.+$");
-    return passwordRegex.hasMatch(password)
-        ? Right(password.trim())
+    final String trimmedPassword = password.trim();
+    final RegExp passwordRegex = RegExp(r"^.+$");
+    return passwordRegex.hasMatch(trimmedPassword)
+        ? Right(trimmedPassword)
         : Left(PasswordValidationFailure());
   }
 
   static Either<ValidationFailure, String> isValidUsername(String username) {
-    if (username.length < 4) return Left(UsernameTooShortValidationFailure());
+    final String trimmedUsername = username.trim();
+    if (trimmedUsername.length < 4)
+      return Left(UsernameTooShortValidationFailure());
 
-    RegExp usernameRegex = RegExp(r"^[a-zA-Z0-9]+$");
-    return usernameRegex.hasMatch(username)
-        ? Right(username)
+    final RegExp usernameRegex = RegExp(r"^[a-zA-Z0-9]+$");
+    return usernameRegex.hasMatch(trimmedUsername)
+        ? Right(trimmedUsername)
         : Left(UsernameValidationFailure());
   }
 }
