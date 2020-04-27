df41dbad78c47fe5572441364ab9eca2ae8204c3
campovski <campovski@gmail.com>
2020-04-27 20:19:56 +0200

    SLCOL-30: Add option for fullscreen CollectioList

--- lib/app/widgets/collectio_list.dart
+++ lib/app/widgets/collectio_list.dart
@@ -7,8 +7,13 @@ import 'circular_network_image.dart';
 class CollectioList<T extends Listable> extends StatelessWidget {
   final List<T> items;
   final void Function(T) onTap;
+  final bool fullScreen;
 
-  const CollectioList({@required this.items, @required this.onTap});
+  const CollectioList({
+    @required this.items,
+    @required this.onTap,
+    this.fullScreen = false,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -29,6 +34,7 @@ class CollectioList<T extends Listable> extends StatelessWidget {
             ),
           )
         : Container(
+            height: fullScreen ? double.infinity : null,
             child: ListView.builder(
               scrollDirection: Axis.vertical,
               shrinkWrap: true,
