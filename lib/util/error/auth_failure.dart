6b7c765dc5d182e3e8d63b2da3db36e84589b650
campovski <campovski@gmail.com>
2020-05-14 10:16:55 +0200

    SLCOL-39: Show AuthFailure on login screen

--- lib/util/error/auth_failure.dart
+++ lib/util/error/auth_failure.dart
@@ -1,3 +1,4 @@
+import '../constant/constants.dart';
 import 'failure.dart';
 
 abstract class AuthFailure extends Failure {
@@ -8,25 +9,32 @@ class InvalidCombinationFailure extends AuthFailure {
   final String email;
   final String password;
 
-  const InvalidCombinationFailure({this.email, this.password, String message})
-      : super(message);
+  const InvalidCombinationFailure({
+    this.email,
+    this.password,
+    String message = Constants.invalidCombination,
+  }) : super(message);
 
   @override
   List<Object> get props => [email, password, message];
 }
 
 class SignOutFailure extends AuthFailure {
-  const SignOutFailure({String message}) : super(message);
+  const SignOutFailure({String message = Constants.cannotSignout})
+      : super(message);
 }
 
 class EmailAlreadyInUseFailure extends AuthFailure {
-  const EmailAlreadyInUseFailure({String message}) : super(message);
+  const EmailAlreadyInUseFailure({String message = Constants.emailInUse})
+      : super(message);
 }
 
 class UsernameAlreadyInUseFailure extends AuthFailure {
-  const UsernameAlreadyInUseFailure({String message}) : super(message);
+  const UsernameAlreadyInUseFailure({String message = Constants.usernameInUse})
+      : super(message);
 }
 
 class ServerFailure extends AuthFailure {
-  const ServerFailure({String message}) : super(message);
+  const ServerFailure({String message = Constants.serverFailure})
+      : super(message);
 }
