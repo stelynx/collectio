43bb7a746c24f428d5f890fcd6d0ccb7f533709e
campovski <campovski@gmail.com>
2020-06-21 09:44:35 +0200

    SLCOL-61: Add logo to sign in screen

--- lib/app/screen/auth/sign_in.dart
+++ lib/app/screen/auth/sign_in.dart
@@ -1,8 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../util/injection/injection.dart';
 import '../../bloc/auth/sign_in_bloc.dart';
+import '../../theme/style.dart';
 import 'widgets/sign_in_form.dart';
 
 class SignInScreen extends StatelessWidget {
@@ -11,7 +14,21 @@ class SignInScreen extends StatelessWidget {
     return Scaffold(
       body: BlocProvider(
         create: (BuildContext context) => getIt<SignInBloc>(),
-        child: SignInForm(),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                'assets/images/collectio_icon.png',
+                height: min(200, MediaQuery.of(context).size.width / 2),
+                width: min(200, MediaQuery.of(context).size.height / 2),
+              ),
+              CollectioStyle.itemSplitter,
+              CollectioStyle.itemSplitter,
+              SignInForm(),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/app/screen/auth/widgets/sign_in_form.dart
+++ lib/app/screen/auth/widgets/sign_in_form.dart
@@ -28,119 +28,114 @@ class SignInForm extends StatelessWidget {
         }
       },
       builder: (context, state) {
-        return Center(
-          child: ListView(
-            shrinkWrap: true,
-            padding: CollectioStyle.screenPadding,
-            children: <Widget>[
-              // Email field
+        return ListView(
+          shrinkWrap: true,
+          padding: CollectioStyle.screenPadding,
+          children: <Widget>[
+            // Email field
+            CollectioTextField(
+              onChanged: (String value) => context
+                  .bloc<SignInBloc>()
+                  .add(EmailChangedSignInEvent(email: value)),
+              labelText:
+                  AppLocalizations.of(context).translate(Translation.email),
+              errorText: state.showErrorMessages && !state.email.isValid()
+                  ? state.email.value.fold(
+                      (ValidationFailure failure) =>
+                          failure is EmailEmptyValidationFailure
+                              ? AppLocalizations.of(context)
+                                  .translate(Translation.fillInEmail)
+                              : AppLocalizations.of(context)
+                                  .translate(Translation.invalidEmail),
+                      (_) => null,
+                    )
+                  : null,
+            ),
+
+            CollectioStyle.itemSplitter,
+
+            // Password field
+            CollectioTextField(
+              obscureText: true,
+              onChanged: (String value) => context
+                  .bloc<SignInBloc>()
+                  .add(PasswordChangedSignInEvent(password: value)),
+              labelText:
+                  AppLocalizations.of(context).translate(Translation.password),
+              errorText: state.showErrorMessages && !state.password.isValid()
+                  ? AppLocalizations.of(context)
+                      .translate(Translation.invalidPassword)
+                  : null,
+            ),
+
+            CollectioStyle.itemSplitter,
+
+            if (state.isRegistering) ...[
+              // Username field
               CollectioTextField(
                 onChanged: (String value) => context
                     .bloc<SignInBloc>()
-                    .add(EmailChangedSignInEvent(email: value)),
-                labelText:
-                    AppLocalizations.of(context).translate(Translation.email),
-                errorText: state.showErrorMessages && !state.email.isValid()
-                    ? state.email.value.fold(
-                        (ValidationFailure failure) =>
-                            failure is EmailEmptyValidationFailure
-                                ? AppLocalizations.of(context)
-                                    .translate(Translation.fillInEmail)
-                                : AppLocalizations.of(context)
-                                    .translate(Translation.invalidEmail),
-                        (_) => null,
-                      )
-                    : null,
-              ),
-
-              CollectioStyle.itemSplitter,
-
-              // Password field
-              CollectioTextField(
-                obscureText: true,
-                onChanged: (String value) => context
-                    .bloc<SignInBloc>()
-                    .add(PasswordChangedSignInEvent(password: value)),
+                    .add(UsernameChangedSignInEvent(username: value)),
                 labelText: AppLocalizations.of(context)
-                    .translate(Translation.password),
-                errorText: state.showErrorMessages && !state.password.isValid()
-                    ? AppLocalizations.of(context)
-                        .translate(Translation.invalidPassword)
+                    .translate(Translation.username),
+                errorText: state.showErrorMessages && !state.username.isValid()
+                    ? state.username.value.fold(
+                        (ValidationFailure failure) =>
+                            failure is UsernameTooShortValidationFailure
+                                ? AppLocalizations.of(context)
+                                    .translate(Translation.usernameTooShort)
+                                : AppLocalizations.of(context)
+                                    .translate(Translation.invalidUsername),
+                        (_) => null)
                     : null,
               ),
 
               CollectioStyle.itemSplitter,
-
-              if (state.isRegistering) ...[
-                // Username field
-                CollectioTextField(
-                  onChanged: (String value) => context
-                      .bloc<SignInBloc>()
-                      .add(UsernameChangedSignInEvent(username: value)),
-                  labelText: AppLocalizations.of(context)
-                      .translate(Translation.username),
-                  errorText: state.showErrorMessages &&
-                          !state.username.isValid()
-                      ? state.username.value.fold(
-                          (ValidationFailure failure) =>
-                              failure is UsernameTooShortValidationFailure
-                                  ? AppLocalizations.of(context)
-                                      .translate(Translation.usernameTooShort)
-                                  : AppLocalizations.of(context)
-                                      .translate(Translation.invalidUsername),
-                          (_) => null)
-                      : null,
-                ),
-
-                CollectioStyle.itemSplitter,
-              ],
-
-              if (state.authFailure != null && state.authFailure.isLeft()) ...[
-                CollectioStyle.itemSplitter,
-                state.authFailure.fold(
-                    (AuthFailure failure) => FailureText(failure.message),
-                    null),
-                CollectioStyle.itemSplitter,
-                CollectioStyle.itemSplitter,
-              ],
-
-              // Sign in with email and password button
-              CollectioButton(
-                onPressed: () => context
-                    .bloc<SignInBloc>()
-                    .add(SignInWithEmailAndPasswordSignInEvent()),
-                text:
-                    AppLocalizations.of(context).translate(Translation.signIn),
-                isPrimary: true,
-              ),
-
-              CollectioStyle.itemSplitter,
-
-              // Sign in with email and password button
-              CollectioButton(
-                onPressed: () {
-                  if (state.isRegistering && state.username.isValid()) {
-                    context
-                        .bloc<SignInBloc>()
-                        .add(RegisterWithEmailAndPasswordSignInEvent());
-                  } else {
-                    context
-                        .bloc<SignInBloc>()
-                        .add(CheckIfEmailExistsSignInEvent());
-                  }
-                },
-                text: AppLocalizations.of(context)
-                    .translate(Translation.register),
-                isPrimary: true,
-              ),
-
-              // Linear progress indicator if submitting the form
-              if (state.isSubmitting) ...[
-                CollectioStyle.itemSplitter,
-                const Center(child: const CircularProgressIndicator()),
-              ],
             ],
-          ),
+
+            if (state.authFailure != null && state.authFailure.isLeft()) ...[
+              CollectioStyle.itemSplitter,
+              state.authFailure.fold(
+                  (AuthFailure failure) => FailureText(failure.message), null),
+              CollectioStyle.itemSplitter,
+              CollectioStyle.itemSplitter,
+            ],
+
+            // Sign in with email and password button
+            CollectioButton(
+              onPressed: () => context
+                  .bloc<SignInBloc>()
+                  .add(SignInWithEmailAndPasswordSignInEvent()),
+              text: AppLocalizations.of(context).translate(Translation.signIn),
+              isPrimary: true,
+            ),
+
+            CollectioStyle.itemSplitter,
+
+            // Sign in with email and password button
+            CollectioButton(
+              onPressed: () {
+                if (state.isRegistering && state.username.isValid()) {
+                  context
+                      .bloc<SignInBloc>()
+                      .add(RegisterWithEmailAndPasswordSignInEvent());
+                } else {
+                  context
+                      .bloc<SignInBloc>()
+                      .add(CheckIfEmailExistsSignInEvent());
+                }
+              },
+              text:
+                  AppLocalizations.of(context).translate(Translation.register),
+              isPrimary: true,
+            ),
+
+            // Linear progress indicator if submitting the form
+            if (state.isSubmitting) ...[
+              CollectioStyle.itemSplitter,
+              const Center(child: const CircularProgressIndicator()),
+            ],
+          ],
         );
       },
     );
